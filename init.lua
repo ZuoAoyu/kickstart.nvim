c4b455c3c6d888d35e670b2b11014a2464881fd3
ZuoGuanghao <zuoguanghao@wanmeixiangsu.cn>
2025-10-20 15:13:43 +0800

    自动切换中英输入法

--- init.lua
+++ init.lua
@@ -805,6 +805,31 @@ require('lazy').setup({
     },
   },
 
+  -- 自动切换中英输入法
+  -- 需要将 fcitx5 的默认输入法设置为 英文
+  {
+    'Kicamon/im-switch.nvim',
+    config = function()
+      require('im-switch').setup {
+        input_toggle = 1, -- 设置为0则默认不自动切换，在insert模式下手动切换为中文后启动自动切换
+        text = { -- 写文档
+          enable = true,
+          files = {
+            '*.md',
+            '*.txt',
+          },
+        },
+        code = { -- 代码注释
+          enable = true,
+          files = { '*' },
+        },
+        en = 'fcitx5-remote -c',
+        zh = 'fcitx5-remote -o',
+        check = 'fcitx5-remote',
+      }
+    end,
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
